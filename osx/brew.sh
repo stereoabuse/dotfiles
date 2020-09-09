730169324cdeefb22a7efdc9a111574e2f0b5906
Chris Bowman <stereoabusestudios@gmail.com>
2020-09-09 13:35:25 -0400

    Add files via upload

---
+++ osx/brew.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install htop
+brew install nmap
+brew install python
+brew install sqlite
+brew install tmux
+brew install vim
+
+#Install Applications
+brew cask install alfred
+brew cask install anaconda
+brew cask install balenaetcher
+brew cask install firefox
+brew cask install google-chrome
+brew cask install google-drive-file-stream
+brew cask install iterm2
+brew cask install itsycal
+brew cask install mendeley
+brew cask install minecraft
+brew cask install processing
+brew cask install pycharm-ce
+brew cask install reaper
+brew cask install spotify
+brew cask install steam
+brew cask install sublime-text
+brew cask install texstudio
+brew cask install tor-browser
+brew cask install tunnelbear
+brew cask install virtualbox
+brew cask install visual-studio-code
+brew cask install vlc
+brew cask install zoom
