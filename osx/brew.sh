5f2c63f52bf6db437fdb157c8bfd62aafaf8a255
Chris B <stereoabusestudios@gmail.com>
2022-02-04 15:52:34 -0500

    change to multiline

--- osx/brew.sh
+++ osx/brew.sh
@@ -1,42 +1,44 @@
 #!/usr/bin/env bash
 
 # Install Brew Packages
-brew install htop
-brew install nmap
-brew install node
-brew install pandoc
-brew install python
-brew install sqlite 
-brew install tmux
-brew install vim
+brew install 
+	htop /
+	nmap /
+	node /
+	pandoc /
+	python /
+	sqlite  /
+	tmux /
+	vim 
 
 #Install Applications
-brew cask install alfred
-brew cask install anaconda
-brew cask install arduino
-brew cask install balenaetcher
-brew cask install discord
-brew cask install firefox
-brew cask install flux
-brew cask install google-chrome
-brew cask install google-drive-file-stream
-brew cask install iterm2
-brew cask install itsycal
-brew cask install julia
-brew cask install lastpass
-brew cask install mendeley
-brew cask install minecraft
-brew cask install processing
-brew cask install pycharm
-brew cask install rectangle
-brew cask install reaper
-brew cask install spotify
-brew cask install steam
-brew cask install sublime-text
-brew cask install texstudio
-brew cask install tor-browser
-brew cask install tunnelbear
-brew cask install virtualbox
-brew cask install visual-studio-code
-brew cask install vlc
-brew cask install zoom
+brew cask install /
+	alfred /
+	anaconda /
+	arduino /
+	balenaetcher /
+	discord /
+	firefox /
+	flux /
+	google-chrome /
+	google-drive-file-stream /
+	iterm2 /
+	itsycal /
+	julia /
+	lastpass /
+	mendeley /
+	minecraft /
+	processing /
+	pycharm /
+	rectangle /
+	reaper /
+	spotify /
+	steam /
+	sublime-text /
+	texstudio /
+	tor-browser /
+	tunnelbear /
+	virtualbox /
+	visual-studio-code /
+	vlc /
+	zoom
