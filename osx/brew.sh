9e9c45a0bb65d1a0fe9b3739799f34bb4089b916
Chris B <stereoabusestudios@gmail.com>
2024-06-18 15:51:51 -0400

    Update brew.sh

--- osx/brew.sh
+++ osx/brew.sh
@@ -1,72 +1,70 @@
 #!/usr/bin/env bash
 
-# if Homebrew is not installed, run:
+# If Homebrew is not installed, run:
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-
 # Install Brew Packages
-brew install 
-	ffmpeg /
-	flac /
-	git /
-	graphviz /
-	htop /
-	imagemagick /
-	lame /
-	neofetch /
-	neovim /
-	nmap /
-	node /
-	openssl@1.1 /
-	pandoc /
-	pipenv /
-	pixman /
-	pkg-config /
-	pypy3 /
-	python@3.12 /
-	r /
-	sqlite /
-	srt /
-	svt-av1 /
-	tcl-tk /
-	tmux /
-	wget /
-	yt-dlp
+brew install \
+  ffmpeg \
+  flac \
+  git \
+  graphviz \
+  htop \
+  imagemagick \
+  lame \
+  neofetch \
+  neovim \
+  nmap \
+  node \
+  openssl@1.1 \
+  pandoc \
+  pipenv \
+  pixman \
+  pkg-config \
+  pypy3 \
+  python@3.12 \
+  r \
+  sqlite \
+  srt \
+  svt-av1 \
+  tcl-tk \
+  tmux \
+  wget \
+  yt-dlp
 
-#Install Applications
-brew cask install /
-	camo-studio /
-	chromium /
-	dropbox /
-	electrum /
-	firefox /
-	flux /
-	github /
-	google-chrome /
-	google-drive /
-	gpg-suite /
-	guitar-pro /
-	iterm2 /
-	logitech-options /
-	mendeley /
-	microsoft-excel /
-	microsoft-powerpoint /
-	microsoft-word /
-	obs /
-	pycharm-ce /
-	reaper /
-	rectangle /
-	spotify /
-	steam /
-	sublime-text /
-	texstudio /
-	the-unarchiver /
-	tor-browser /
-	transmission /
-	tunnelbear /
-	ultimaker-cura /
-	virtualbox /
-	visual-studio-code /
-	vlc /
-	zoom
-
+# Install Applications
+brew install --cask \
+  camo-studio \
+  chromium \
+  dropbox \
+  electrum \
+  firefox \
+  flux \
+  github \
+  google-chrome \
+  google-drive \
+  gpg-suite \
+  guitar-pro \
+  iterm2 \
+  logitech-options \
+  mendeley \
+  microsoft-excel \
+  microsoft-powerpoint \
+  microsoft-word \
+  obs \
+  pycharm-ce \
+  reaper \
+  rectangle \
+  spotify \
+  steam \
+  sublime-text \
+  texstudio \
+  the-unarchiver \
+  tor-browser \
+  transmission \
+  tunnelbear \
+  ultimaker-cura \
+  virtualbox \
+  visual-studio-code \
+  vlc \
+  zoom
